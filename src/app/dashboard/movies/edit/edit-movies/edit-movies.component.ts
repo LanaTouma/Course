4db9132dd88854753cf483864bb8167439f2892a
Aya-kalfa <AyaKalfa1999@gmail.com>
2023-10-26 12:26:29 -0700

    This is the push from Aya

---
+++ src/app/dashboard/movies/edit/edit-movies/edit-movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-movies',
+  templateUrl: './edit-movies.component.html',
+  styleUrls: ['./edit-movies.component.scss']
+})
+export class EditMoviesComponent {
+
+}
